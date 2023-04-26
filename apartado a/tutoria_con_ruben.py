435cc4e632a2f3a76cb78969c1ae9cbd3cdd0af2
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-26 17:10:54 +0200

    300 datos en el dataset

--- apartado a/tutoria_con_ruben.py
+++ apartado a/tutoria_con_ruben.py
@@ -24,7 +24,7 @@ def generate_sample():
         "2do más probable": second_most_probable_planet
     }
 
-dataset = [generate_sample() for _ in range(100)]
+dataset = [generate_sample() for _ in range(300)]
 
 # Crear el DataFrame con el dataset
 df = pd.DataFrame(dataset)
