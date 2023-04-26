e2f60be39e5bb0946ca6f1a194513cfff53bb488
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-26 10:10:53 +0200

    importar

---
+++ apartado a/dataset.py
@@ -0,0 +1,6 @@
+#crearemos a continuacion un dataset sobre las pruebas al lanzar una bola en un espacio de 1 metro
+
+import numpy as np
+import pandas as pd
+
+#creamos un dataset con 1000 filas y 3 columnas
