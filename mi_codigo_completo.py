0a9c260815516644d299fe5d808ba67bc465d4ab
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-28 18:16:14 +0200

    histogramas

--- mi_codigo_completo.py
+++ mi_codigo_completo.py
@@ -2,6 +2,7 @@ import random
 from faker import Faker
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
 
 faker = Faker()
 
@@ -27,4 +28,21 @@ for i in range(1903):
     dataset.append(row)
 
 df = pd.read_csv('datos_gravedad.csv')
+
+
+colors = {'Tierra': 'blue', 'Saturno': 'red', 'Urano': 'green', 'Neptuno': 'purple'}
+
+scatter_matrix(df, figsize=(10, 10), diagonal='kde', color=df['Planeta'].apply(lambda x: colors[x]))
+plt.show() #Creamos una matriz de dispersion
+# creacion de histogramas
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 10))
+
+for ax, col in zip(axes.flatten(), df.columns[:-1]):
+    ax.hist(df[col], bins=10, color=colors[df['Planeta']], alpha=0.5)
+    ax.set_xlabel(col)
+    ax.set_ylabel('Frecuencia')
+
+plt.tight_layout()
+plt.show()
+
 
