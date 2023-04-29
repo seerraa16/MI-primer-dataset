ce32b58ada692ceb8d815bc9b5ffff61af58ec2d
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-29 12:08:00 +0200

    sample

--- mi_codigo_completo.py
+++ mi_codigo_completo.py
@@ -10,24 +10,24 @@ planets = ['Tierra', 'Marte', 'Júpiter', 'Saturno', 'Urano', 'Neptuno']
 accelerations = [9.8, 3.7, 24.8, 9.0, 8.7, 11.0]
 
 dataset = []
-
-for i in range(1903):
-    L = random.uniform(0.5, 2.0)
-    t = random.uniform(0.3, 0.8)
-    planet = random.choice(planets)
-    g = accelerations[planets.index(planet)] + random.uniform(-0.5, 0.5)
-    error = random.uniform(0.01, 0.1)
-    most_probable_planet = planets[accelerations.index(min(accelerations, key=lambda x: abs(x - g)))]
-    second_most_probable_planet = planets[accelerations.index(max(accelerations, key=lambda x: abs(x - g)))]
-    row = {
-        'ID': faker.uuid4(),
-        'L (m)': round(L, 2),
-        't (s)': round(t, 2),
-        'Planet': planet,
-        'g (m/s^2)': round(g, 2),
-        'Error': round(error, 2)
-    
-    }
+def generate_sample():
+    for i in range(1903):
+        L = random.uniform(0.5, 2.0)
+        t = random.uniform(0.3, 0.8)
+        planet = random.choice(planets)
+        g = accelerations[planets.index(planet)] + random.uniform(-0.5, 0.5)
+        error = random.uniform(0.01, 0.1)
+        most_probable_planet = planets[accelerations.index(min(accelerations, key=lambda x: abs(x - g)))]
+        second_most_probable_planet = planets[accelerations.index(max(accelerations, key=lambda x: abs(x - g)))]
+        row = {
+            'ID': faker.uuid4(),
+            'L (m)': round(L, 2),
+            't (s)': round(t, 2),
+            'Planet': planet,
+            'g (m/s^2)': round(g, 2),
+            'Error': round(error, 2)
+        
+        }
     dataset.append(row)
 
 df = pd.DataFrame(dataset)
